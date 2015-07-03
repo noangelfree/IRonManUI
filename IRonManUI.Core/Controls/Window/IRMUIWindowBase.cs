5b9ff6be68bc787e2db091bf92e872db211d6e4f
noangelfree <noangelfree@gmail.com>
2015-07-03 23:17:00 +0300

    Add project

---
+++ IRonManUI.Core/Controls/Window/IRMUIWindowBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace IRonManUI.Core.Controls.Window
+{
+    public class IRMUIWindowBase : System.Windows.Window
+    {
+       public IRMUIWindowBase()
+        {
+            this.Style = (Style)FindResource("IRMUIWindowStyle");
+        }
+    }
+}
